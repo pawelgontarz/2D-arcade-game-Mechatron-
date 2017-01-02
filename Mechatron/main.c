79896c091107d68320239948923ed43628893c1b
Mac Paweł <MacBookPRO@MacBook-Pro-Mac.local>
2017-01-02 10:42:55 +0100

    Initial Commit

---
+++ Mechatron/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Mechatron
+//
+//  Created by Mac Paweł on 02.01.2017.
+//  Copyright © 2017 Macbook Pawel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
